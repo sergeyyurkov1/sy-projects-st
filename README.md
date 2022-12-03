509420d34469fdfb16c38900a17315751d8225f6
Sergey Yurkov <38420364+sergeyyurkov1@users.noreply.github.com>
2022-12-03 18:57:44 +0500

    1

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ conda create -n multiapp python=3.7.9
 conda activate multiapp
 
 pip install -r requirements.txt
+pip install protobuf==3.20.\*
 
 streamlit run app.py
--- apps/anki_to_obsidian.py
+++ apps/anki_to_obsidian.py
@@ -12,9 +12,9 @@ import time
 def app():
     st.title("Anki to Obsidian exporter")
     st.subheader("Export your flashcards to Obsidian note taking app.")
-    st.markdown("***")
+    # st.markdown("***")
 
-    col1, col2 = st.columns((1, 2))
+    col1, col2 = st.columns((1, 1))
 
     with col1:
         options = st.expander("Options", False)
@@ -30,6 +30,7 @@ def app():
     #     )
     #     # fmt: on
 
+
     with col2:
         uploaded_file = st.file_uploader(
             "Choose your export file",
@@ -39,15 +40,18 @@ def app():
             help=None,
         )
 
-        if uploaded_file is not None:
+    st.markdown("***")
 
+    col3, col4 = st.columns((1, 1))
+
+    if uploaded_file is not None:
+        with col3:
             with st.spinner(text="Loading preview..."):
                 stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
                 string_data = stringio.read()
                 stringio.close()
 
-                st.markdown("***")
-                st.header("Data preview")
+                st.header("Before preview")
                 st.write(string_data.split("\n")[:5])
 
             if re.search(r"^(.*)\t(.*)\t(.*)", string_data) == None:
@@ -63,27 +67,26 @@ def app():
 
             st.success("Conversion success")
 
-            with st.spinner(text="Loading preview..."):
+        with col4:
+            with st.spinner(text="Preparing download..."):
                 time.sleep(4)
-
-                st.markdown("***")
-                st.header("Download preview")
-                st.write(out.split("\n\n")[:5])
 
                 output = StringIO()
                 output.write(out)
 
                 o = output.getvalue()
 
-            with st.spinner(text="Preparing download..."):
-                time.sleep(4)
-
                 b64 = base64.b64encode(
                     o.encode()
                 ).decode()  # some strings <-> bytes conversions necessary here
                 href = f'<a href="data:file/txt;base64,{b64}" download="anki-{option.lower()}.md">Download Markdown</a>'
 
-                st.markdown("***")
                 st.markdown(href, unsafe_allow_html=True)
 
                 output.close()
+
+            with st.spinner(text="Loading preview..."):
+                time.sleep(4)
+
+                st.header("After preview")
+                st.write(out.split("\n\n")[:5])
